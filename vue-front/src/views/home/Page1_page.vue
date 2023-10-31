5f14880ad1db4a25ec4327d082bfbe9d3b2c9658
bankthanakit <thanakit.ki@inet.co.th>
2023-10-31 14:38:12 +0700

    add function delete

--- vue-front/src/views/home/Page1_page.vue
+++ vue-front/src/views/home/Page1_page.vue
@@ -1,46 +1,90 @@
-<template>
-    <div>
-        <v-data-table v-bind:headers="headers" :items="dataApi"></v-data-table>
-    </div>
-</template>
+    <template>
+        <div>
+            <v-data-table v-bind:headers="headers" :items="dataApi">
+                <template v-slot:item="row">
+                    <tr>
+                        <td v-if="!row.isEditing">{{row.item.title}}</td>
+                        <td v-if="!row.isEditing">{{row.item.price}}</td>
 
-<script>
-export default {
-    data(){
-        return {
-            dataApi: [],
-            headers: [{
-                text: 'Title',
-                align: 'start',
-                sortable: false,
-                value: 'title'
-                },
-                {
-                text: 'Price',
-                align: 'start',
-                sortable: false,
-                value: 'price'
+                        <!-- <td v-if="row.isEditing"><v-text-field v-model="row.item.title" label="Title"></v-text-field></td>
+                        <td v-if="row.isEditing"><v-text-field v-model="row.item.price" label="Price" type="number"></v-text-field></td> -->
+                        
+                        <td v-if="!row.isEditing">
+                            <v-btn @click="edit_book(row.item)">edit</v-btn>
+                            <v-btn @click="delete_book(row.item)">delete</v-btn>
+                        </td>
+                        <!-- <td v-if="row.isEditing">
+                            <v-btn @click="edit_book(row.item)">save</v-btn>
+                            <v-btn @click="toggleEdit(row.edit_mode)">cancel</v-btn>
+                        </td> -->
+                        
+                    </tr>
+                </template>
+            </v-data-table>
+        </div>
+    </template>
+
+    <script>
+    export default {
+        data(){
+            return {
+                edit_mode: [],
+                dataApi: [],
+                headers: [{
+                    text: 'Title',
+                    align: 'start',
+                    sortable: false,
+                    value: 'title'
+                    },
+                    {
+                    text: 'Price',
+                    align: 'start',
+                    sortable: false,
+                    value: 'price'
+                    },
+                    {
+                        text: '',
+                    }
+                ]
+            }
+        },
+        created(){
+            const options={
+                url:'http://localhost:3000/books',
+                method:'get',
+            }
+
+            this.axios(options)
+                .then((res) => {
+                    this.dataApi = res.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+        methods: {
+            edit_book(item) {
+                console.log(item.id);
+            },
+            delete_book(item) {
+                const options={
+                url:'http://localhost:3000/delete/'+item.id,
+                method:'delete',
                 }
-            ]
-        }
-    },
-    created(){
-        const options={
-            url:'http://localhost:3000/books',
-            method:'get',
-        }
+                this.axios(options)
+                .then((res) => {
+                    this.dataApi = res.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+                window.location.reload();
+            },
 
-        this.axios(options)
-            .then((res) => {
-                this.dataApi = res.data;
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        }
     }
-}
-</script>
+    </script>
 
-<style lang="scss" scoped>
+    <style lang="scss" scoped>
 
-</style>
+    </style>
--- vue-front/src/views/home/Page2_page.vue
+++ vue-front/src/views/home/Page2_page.vue
@@ -1,15 +1,42 @@
 <template>
     <div>
-        page2
+        <h1>Add Book</h1>
+        <v-text-field label="Title" v-model="payload.title"></v-text-field>
+        <v-text-field label="Price" v-model="payload.price" type="number"></v-text-field>
+        <v-btn @click="add">Add</v-btn>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
+    data(){
+        return {
+            payload: {
+                title: '',
+                price: null
+            }
+        }
+    },
+    methods: {
+        async add(){
+            const price = parseInt(this.payload.price, 10);
+            this.payload.price = price;
+            const options={
+                url:'http://localhost:3000/book',
+                method:'post',
+                data: this.payload
+            }
+            this.axios(options)
+                .then((res) => {
+                    console.log(res);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
 
-        return {}
+            this.payload.title = '';
+            this.payload.price = null;
+        }
     }
 }
 </script>
